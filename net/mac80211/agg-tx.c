e57d2968042142bc65e5767f748ceac7fd0fb4fa
Kiki Rizky <kiki.rizky82@gmail.com>
2012-02-29 18:17:50 +0700

    updated to 2.6.32.13
    Conflicts:
    
    	sound/pci/hda/hda_intel.c

--- net/mac80211/agg-tx.c
+++ net/mac80211/agg-tx.c
@@ -180,7 +180,6 @@ static void sta_addba_resp_timer_expired(unsigned long data)
 	if ((*state & (HT_ADDBA_REQUESTED_MSK | HT_ADDBA_RECEIVED_MSK)) !=
 						HT_ADDBA_REQUESTED_MSK) {
 		spin_unlock_bh(&sta->lock);
-		*state = HT_AGG_STATE_IDLE;
 #ifdef CONFIG_MAC80211_HT_DEBUG
 		printk(KERN_DEBUG "timer expired on tid %d but we are not "
 				"(or no longer) expecting addBA response there",
