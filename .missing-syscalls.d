17b631112b3303b1684f0da964b443884a523e4d
Keiran Collins <keiran@darkforestgroup.com>
2012-06-05 18:38:04 +0100

    pushed FXP sources

---
+++ .missing-syscalls.d
@@ -0,0 +1,6 @@
+-:  include/linux/autoconf.h \
+ /home/keiran/semc_kernel_src/arch/arm/include/asm/types.h \
+ include/asm-generic/int-ll64.h \
+ /home/keiran/semc_kernel_src/arch/arm/include/asm/bitsperlong.h \
+ include/asm-generic/bitsperlong.h \
+ /home/keiran/semc_kernel_src/arch/arm/include/asm/unistd.h
